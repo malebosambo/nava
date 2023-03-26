3a5f8ad28c994697b356dfbbe85dd38ee7e353cb
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-03-26 17:19:27 +0200

    Updating design

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       </div>
       <div className="Main_Content">
         <div className="Harvest">
-          <h2>Harvesting</h2>
+          <h1>Harvesting</h1>
           <p>Navah grows and produces vegetables and herbs at its farm for the agriculture market.</p>
         </div>
         <div className="Product">
-          <h3>Organic Okra</h3>
+          <h2>Organic Okra</h2>
           <img src="./okra.jpg" alt="okra" />
           <p className="Bold">R50.00/kg</p>
         </div>
@@ -20,6 +20,9 @@ function App() {
           <h2>Driven by sustainability</h2>
           <p>We use sustainability farming methodology to make a positive contribution to the environment.</p>
         </div>
+        <div className="Health">
+          <h1>Healthy Living</h1>
+          <p>Live a life worthwhile with fewer sick days.</p>
       </div>
       <div className="Footer">
         <p>© 2023, Navah Harvest & Health</p>
