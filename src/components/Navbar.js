74dd3b6a1c403612b8a824df0eefadf71ea59179
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-04-15 18:54:20 +0200

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { useState } from 'React';
+import '../styles/navbar.css';
+
+export default function() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  
+  return (
+    <nav className="Navigation">
+      <a href="/" className="Logo">Navah Harvest & Health</a>
+      <button className="Menu" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Menu</button>
+      <div className={ isNavExpanded ? "Navigation-Menu Expanded" : "Navigation-Menu" }>
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
