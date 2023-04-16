9c1c1f0f4c99eab961ceb81da24ac96d91357bf7
malebo.sambo@outlook.com <malebo.sambo@outlook.com>
2023-04-16 12:30:11 +0200

    Navbar not working. Made minor changes to evaluate if Navbar is mounted.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,11 @@ import { useState } from 'React';
 import '../styles/navbar.css';
 
 export default function() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   
   return (
     <nav className="Navigation">
-      <a href="/" className="Logo">Navah Harvest & Health</a>
+      <a href="/" className="Logo">Navah Harvest</a>
       <button className="Menu" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Menu</button>
       <div className={ isNavExpanded ? "Navigation-Menu Expanded" : "Navigation-Menu" }>
         <ul>
